1f84df97d8f274522c6d4297dfde046f0a3ee639
rkihacker <rkihacker@gmail.com>
2024-10-09 15:52:25 +0500

    Update option.go

--- controller/option.go
+++ controller/option.go
@@ -37,7 +37,7 @@ func UpdateOption(c *gin.Context) {
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"success": false,
-			"message": "无效的参数",
+			"message": "Invalid parameters",
 		})
 		return
 	}
@@ -46,7 +46,7 @@ func UpdateOption(c *gin.Context) {
 		if option.Value == "true" && common.GitHubClientId == "" {
 			c.JSON(http.StatusOK, gin.H{
 				"success": false,
-				"message": "无法启用 GitHub OAuth，请先填入 GitHub Client Id 以及 GitHub Client Secret！",
+				"message": "Unable to enable GitHub OAuth, please fill in GitHub Client Id and GitHub Client Secret first!",
 			})
 			return
 		}
@@ -54,7 +54,7 @@ func UpdateOption(c *gin.Context) {
 		if option.Value == "true" && len(common.EmailDomainWhitelist) == 0 {
 			c.JSON(http.StatusOK, gin.H{
 				"success": false,
-				"message": "无法启用邮箱域名限制，请先填入限制的邮箱域名！",
+				"message": "Unable to enable email domain restriction, please fill in the restricted email domain first!",
 			})
 			return
 		}
@@ -62,7 +62,7 @@ func UpdateOption(c *gin.Context) {
 		if option.Value == "true" && common.WeChatServerAddress == "" {
 			c.JSON(http.StatusOK, gin.H{
 				"success": false,
-				"message": "无法启用微信登录，请先填入微信登录相关配置信息！",
+				"message": "Unable to enable WeChat login, please fill in the WeChat login related configuration information first!",
 			})
 			return
 		}
@@ -70,7 +70,7 @@ func UpdateOption(c *gin.Context) {
 		if option.Value == "true" && common.TurnstileSiteKey == "" {
 			c.JSON(http.StatusOK, gin.H{
 				"success": false,
-				"message": "无法启用 Turnstile 校验，请先填入 Turnstile 校验相关配置信息！",
+				"message": "Unable to enable Turnstile verification, please fill in Turnstile verification related configuration information first!",
 			})
 			return
 		}
